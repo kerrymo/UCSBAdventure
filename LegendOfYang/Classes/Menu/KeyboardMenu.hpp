e9fcc94cd47564033d410642dbc44167a4f3848e
John Rehbein <jkr@umail.ucsb.edu>
2018-02-07 12:26:30 -0800

    Made a slight change to Keyboard menu that I forgot to make

--- LegendOfYang/Classes/Menu/KeyboardMenu.hpp
+++ LegendOfYang/Classes/Menu/KeyboardMenu.hpp
@@ -5,24 +5,44 @@
 //  Created by John Rehbein on 2/4/18.
 //
 
-#ifndef KeyboardMenu_hpp
-#define KeyboardMenu_hpp
+#pragma once
 
 USING_NS_CC;
 
 // Class Description - This class displays a vertical box of options you can select
 // Use Cases - Action Selecting during a battle, Selecting Save file, Selecting Item from inventory, etc.
 
-typedef std::pair<std::string, std::function<void(std::string)>> LabelAndCallback;
+/* ExampleCode -
+ std::vector<LabelAndCallback> items;
+ LabelAndCallback item1, item2;
+ item1.first = "Option 1";
+ item2.first = "Option 2";
+ item1.second = CC_CALLBACK_1(MyClassName::callBackFunction1, this);
+ item2.second = CC_CALLBACK_1(MyClassName::callBackFunction2, this);
+ items.push_back(item1);
+ items.push_back(item2);
+ auto menu = KeyboardMenu::create(items);
+ guiNode->addChild(menu);
+ 
+ // Somewhere in your class
+ void MyClassName::callBackFunction1(Node *keyboardMenu) {
+     // Handle callback 1
+ }
+ void MyClassName::callBackFunction2(Node *keyboardMenu) {
+     // Handle callback 2
+ }
+ */
+
+typedef std::pair<std::string, std::function<void(Node*)>> LabelAndCallback;
 
 class KeyboardMenu : public Node {
 public:
     // Interface functions
+    std::string selectedLabelText() { return _items.at(selectedItem).first; };
     void setContentSize(const cocos2d::Size &contentSize) override;
     
     // Initialization functions
     virtual bool init() override;
-    // create a create() function that calls constructor, init and autorelease
     CREATE_FUNC(KeyboardMenu);
     static KeyboardMenu* create(const std::vector<LabelAndCallback> &items);
     static KeyboardMenu* create(const std::vector<LabelAndCallback> &items, const Size &contentSize);
@@ -32,5 +52,5 @@ protected:
     void updateDisplay();
     int selectedItem;
     std::vector<LabelAndCallback> _items;
+    
 };
-#endif /* KeyboardMenu_hpp */
